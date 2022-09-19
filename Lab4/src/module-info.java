f256af2adbe01d10a6d84d34ae9d0166bfaf5ff3
Ananya Bundela <99980162+ananya36@users.noreply.github.com>
2022-09-19 10:18:37 +0530

    Folder added

---
+++ Lab4/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ananya Bundela
+ *
+ */
+module Lab4 {
+}
---
+++ Project1/src/AIML.java
@@ -0,0 +1,9 @@
+
+public class AIML extends Object {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello World");
+	}
+
+}
