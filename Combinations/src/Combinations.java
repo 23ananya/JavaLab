48ca7273f104315133b6fbef614441c7ac6b95ac
23ananya <113884392+23ananya@users.noreply.github.com>
2022-09-19 12:46:05 +0530

    Update Combinations.java

--- Combinations/src/Combinations.java
+++ Combinations/src/Combinations.java
@@ -2,7 +2,22 @@
 public class Combinations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			
+	public static void main(String[] args) {
+	Scanner scan=new Scanner (System.in);
+	System.out.println("Enter the three digits from 0 to 9: ");
+	String a=scan.nextLine();
+	String b=scan.nextLine();
+	String c=scan.nextLine();
+        System.out.println("The possible combinations are: ");
+	System.out.println(a+b+c);
+	System.out.println(b+a+c);
+	System.out.println(c+b+a);
+	System.out.println(b+c+a);
+	System.out.println(c+a+b);
+	System.out.println(a+c+b);
+
 
 	}
 
