97bfe0633be39dd20a3a5626d78f5cdb341ae62b
23ananya <113884392+23ananya@users.noreply.github.com>
2022-09-22 16:35:37 +0530

    Update Sum.java

--- Sum/src/Sum.java
+++ Sum/src/Sum.java
@@ -1,19 +1,12 @@
 
-public class Sum {
-
+public class Sum{
 	public static void main(String[] args) {
-		int i;
 		int sum=0;
-		for(i=40;i<250;i++);{
-		
-			if(i%5==0){
-				System.out.println(i);
+		for(int i=41;i<250;i++) {
+			if(i%5==0) {
 				sum=sum+i;
-				
 			}
-}
+		}
 		System.out.println(sum);
-
+	}
 }
-}
-	
